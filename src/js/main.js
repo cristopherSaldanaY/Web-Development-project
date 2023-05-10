2e535ca73b8fbe9aab32f42b88f2a16d841b08de
cristopherSaldanaY <cristopher.saldanay@gmail.com>
2023-05-10 02:36:33 -0400

    blog

--- src/js/main.js
+++ src/js/main.js
@@ -34,20 +34,45 @@ const mapDataToCards = (data) => {
     (item) => `
 
     <div class="blog-card-item">
-        <figure><img src="${item.imageCourse}" alt=""></figure>
+        <figure><img class="image-blog" src="${item.imageCourse}" alt=""></figure>
         <div class="card-item-content">
             <div class="item-content-title">
                 <p>${item.tecnology}</p>
                 <h2>${item.title}</h2>
             </div>
             <p>${item.description}</p>
-            <div class="btn-blog-item">see the post</div>
+            <div class="btn-blog-item" data-blog-id="${item.id}">see the post</div>
         </div>
     </div>
     `
   );
 };
 
+/* mapping data blogs */
+const mapDataToBlogDetailed = (data, blogId) => {
+  const blog = data.find((item) => item.id === blogId);
+
+  if (!blog) {
+    return "<p>The selected blog was not found.</p>";
+  }
+
+  const { title, subtitle, image, description, information } = blog;
+  const { institution, date, instructor } = information;
+
+  const otherBlogs = data.filter((item) => item.id !== blogId);
+  const otherBlogsHtml = otherBlogs
+    .map((blog) => {
+      return `<p><a href="blog.html?id=${blog.id}">${blog.title}</a></p>`;
+    })
+    .join("");
+
+
+
+
+  return ``
+};
+
+/* insert DOM blogs */
 const insertCardsToDOM = () => {
   const template = mapDataToCards(dataPosts);
 
@@ -57,3 +82,21 @@ const insertCardsToDOM = () => {
 if (window.location.pathname.includes("blogs.html")) {
   insertCardsToDOM();
 }
+
+/* blog.html */
+const goToBlog = (e) => {
+  const blogId = e.currentTarget.getAttribute("data-blog-id");
+  window.location.href = `blog.html?id=${blogId}`;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const goPostButtons = document.querySelectorAll(".btn-blog-item");
+
+  goPostButtons.forEach((button) => {
+    button.addEventListener("click", goToBlog);
+  });
+});
+
+
+const parrafo = document.getElementById('parrafo');
+parrafo.innerHTML = parrafo.innerHTML.replace(/\./g, '.<br>');
--- src/utils/data.js
+++ src/utils/data.js
@@ -4,21 +4,24 @@ const dataPosts = [
     imageCourse: "./src/assets/image-blog.png",
     tecnology: "NodeJS",
     title: "Curso NodeJS",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
   },
   {
     id: 2,
     imageCourse: "./src/assets/image-blog.png",
     tecnology: "Java",
     title: "Java Fundamentals",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
   },
   {
     id: 3,
     imageCourse: "./src/assets/image-blog.png",
     tecnology: "HTML, CSS y JavaScript",
     title: "Web Development",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
   },
 ];
 
@@ -29,7 +32,7 @@ const dataPostsDetail = [
     image: "/src/assets/nodejs3.png",
     subtitle: "Node JS - TypeScript",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.",
     information: {
       institution: "Escalab Tech Hub",
       date: "01/03/2023",
@@ -42,7 +45,7 @@ const dataPostsDetail = [
     image: "/src/assets/java.png",
     subtitle: "Node JS - TypeScript",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.",
     information: {
       institution: "Escalab Tech Hub",
       date: "01/03/2023",
@@ -55,7 +58,7 @@ const dataPostsDetail = [
     image: "/src/assets/html-css-javascript.png",
     subtitle: "Node JS - TypeScript",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a.",
     information: {
       institution: "Escalab Tech Hub",
       date: "01/03/2023",
